c1707f2f7a4b191602ef965a6ac989aa54b49d00
KerlDrey <davsyanikov1@gmail.com>
2023-05-06 17:24:04 +0300

    HomePage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import HeaderContainer from './components/Header/HeaderComponent';
+import HomePageContainer from './components/HomePage/HomePageContainer';
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <div>
+        <HeaderContainer />
+        <HomePageContainer />
+      </div>
     </div>
   );
 }
