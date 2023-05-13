613a00c94b3f65ca91a04a8ce646274559afd955
KerlDrey <davsyanikov1@gmail.com>
2023-05-13 13:06:58 +0300

    Refactoring

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import { DropdownButton, Form, Image, Dropdown, ButtonGroup, Button } from "react-bootstrap";
-=======
-import { Button, Form, Image } from "react-bootstrap";
->>>>>>> 18a67cfd3a97f38869adcbaf912f5d44edf8f8e3
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -23,11 +19,12 @@ import { StoreContext } from "../..";
 import { fetchCategoriesTC } from "../../Redux/ActionCreators/categoryAC";
 import { useEffect } from "react";
 import { useState } from "react";
-<<<<<<< HEAD
+
 import DropdownItem from "react-bootstrap/esm/DropdownItem";
-=======
 import { logoutAC } from "../../Redux/ActionCreators/authAC";
->>>>>>> 18a67cfd3a97f38869adcbaf912f5d44edf8f8e3
+import DropdownToggle from "react-bootstrap/esm/DropdownToggle";
+import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
+
 
 
 const Header = () => {
@@ -59,26 +56,21 @@ const Header = () => {
   return (
     <header>
       <Container className={s.container}>
-<<<<<<< HEAD
         <Row>
-          <Col className={s.upper_header}>
-            <Link to={REGISTRATION_ROUTE}>Регистрация</Link>
-            <Link to={LOGIN_ROUTE} style={{textDecoration: 'none'}}><Button size="sm" variant="success" id={s.enter_button}>Войти</Button></Link>
-          </Col>
-=======
-        <Row className={s.upper_header}>
-          <Col md={10}></Col>
-          {isAuth ?
-           
-             <Col xs={2}><Button onClick={logout}>Выйти</Button></Col>
-         :
-          <>
-           <Col md={{ span: 2, offset: 2 }}><Link to={REGISTRATION_ROUTE}>Регистрация</Link></Col>
-           <Col md={{ span: 2, offset: 2 }}><Link to={LOGIN_ROUTE}>Войти</Link></Col>
-           </>
-          }
-         
->>>>>>> 18a67cfd3a97f38869adcbaf912f5d44edf8f8e3
+          {isAuth ? (
+            <Col className={s.upper_header}>
+              <Button size="sm" onClick={logout}>
+                Выйти
+              </Button>
+            </Col>
+          ) : (
+            <>
+              <Col className={s.upper_header}>
+                <Link to={REGISTRATION_ROUTE}>Регистрация</Link>
+                <Link to={LOGIN_ROUTE}>Войти</Link>
+              </Col>
+            </>
+          )}
         </Row>
         <Row className={s.lower_header}>
           <Col xs={2}>
@@ -90,19 +82,17 @@ const Header = () => {
             <Link to={NEW_QUESTION_ROUTE}>Спросить</Link>
           </Col>
           <Col xs={3}>
-            <DropdownButton variant="dark">
+            <Dropdown>
+              <DropdownToggle variant="success" id="dropdown-basic">
+                Категории
+              </DropdownToggle>
+              <DropdownMenu>
               {categories.map((category)=>(
                 <DropdownItem eventKey={category.id}>{category.name}</DropdownItem>
               ))}
-            </DropdownButton>
-          {/* <select className={s.select_list} name="Категории">
-              {
-                categories.map((category)=>(
-                  <option value={category.id}>{category.name}</option>
-                ))
-              }
-            </select> */}
-            </Col>
+              </DropdownMenu>
+            </Dropdown>
+          </Col>
           <Col xs={2}>
             <Link to={RULES_PAGE_ROUTE}>Правила</Link>
           </Col>
